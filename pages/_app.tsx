8ab636e5f3738100ff1a5f925ebc271fbce16ae0
Summer Chen <mp4bj6ruby@gmail.com>
2021-01-29 15:37:45 +0800

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+export default MyApp
