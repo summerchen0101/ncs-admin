3e780d5e0432727f283aaf188198354deb6d3530
Summer Chen <mp4bj6ruby@gmail.com>
2021-02-17 22:23:37 +0800

    clean up apiErrHandler

--- utils/useErrorHandler.ts
+++ utils/useErrorHandler.ts
@@ -8,50 +8,25 @@ function useErrorHandler() {
   const toast = useToast()
   const router = useRouter()
 
-  const errCodeHandler = (code: number) => {
-    if (!code) return
-    throw new Error(errCodes[code] || '操作錯誤')
-  }
   const apiErrHandler = (error: AxiosError<any>) => {
-    // console.log(error)
     if (error.response) {
+      // 錯誤來自回傳參數
       if (error.response.status === 401) {
         router.push('/login')
       }
       toast({ title: httpStatus[error.response.status], status: 'error' })
-      console.log(error.response.data)
     } else if (error.request) {
+      // 錯誤來自請求參數
       console.log(error.request)
     } else {
-      console.log('Error', error.message)
+      // 錯誤來自其他因素
       toast({ title: error.message, status: 'error' })
     }
     // console.log(error.config)
   }
   return {
     apiErrHandler,
-    errCodeHandler,
   }
-
-  // const router = useRouter()
-  // const toast = useToast()
-  // let errorMsg = ''
-  // if (res.data.code) {
-  //   errorMsg = errCodes[res.data.code]
-  // } else if (res.status === 401) {
-  //   router.push('/login')
-  //   errorMsg = httpStatus[401]
-  // } else if (res.status === 500) {
-  //   errorMsg = '系統錯誤'
-  // } else if (res.data.error) {
-  //   errorMsg = '操作錯誤'
-  // }
-  // if (errorMsg) {
-  //   toast({
-  //     title: errorMsg,
-  //   })
-  //   throw new Error(errorMsg)
-  // }
 }
 
 export default useErrorHandler
