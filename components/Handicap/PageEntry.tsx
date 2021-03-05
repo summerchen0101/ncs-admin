d6cb19d7cd2fef6cf04ce61b5daab1014b6a5a5e
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-05 23:25:22 +0800

    Handicap: use static gameOpts
    (cherry picked from commit 25ede03d3529e5b4e1d582f2415dc80fe45b869f)

--- components/Handicap/PageEntry.tsx
+++ components/Handicap/PageEntry.tsx
@@ -3,19 +3,13 @@ import PageSearchBar from '@/components/Handicap/PageSearchBar'
 import TableData from '@/components/Handicap/TableData'
 import { useDataContext } from '@/context/DataContext'
 import { Handicap } from '@/types/api/Handicap'
-import useOptionsService from '@/utils/services/useOptionsService'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Dashboard from '../Dashboard'
 import Paginator from '../Paginator'
 import ScorePopup from './ResultPopup'
 
 const PageEntry: React.FC = () => {
   const { list } = useDataContext<Handicap>()
-  const { fetchGameOptions } = useOptionsService()
-
-  useEffect(() => {
-    fetchGameOptions()
-  }, [])
 
   return (
     <Dashboard>
