c110852f7e634327c121b6b3877548588d3167d4
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-07 00:58:48 +0800

    menu order updated

--- lib/menu.ts
+++ lib/menu.ts
@@ -21,7 +21,7 @@ type MenuTree = {
 }
 
 const menu: MenuTree = {
-  home: { name: '首页', icon: 'HiOutlineStar', path: '/' },
+  home: { name: '首页', icon: 'HiOutlineHome', path: '/' },
   user: {
     name: '個人管理',
     icon: 'HiOutlineUser',
@@ -32,66 +32,15 @@ const menu: MenuTree = {
       shadow: { name: '子帳號', path: '/user/shadow' },
     },
   },
-  admin: {
-    name: '管理员管理',
-    icon: 'HiOutlineStar',
-    path: '/admin',
-    pages: {
-      adminUser: { name: '管理员列表', path: '/admin/user' },
-      adminRole: { name: '角色管理', path: '/admin/role' },
-      menu: { name: '选单管理', path: '/admin/menu', menuHidden: true },
-    },
-  },
   announce: {
     name: '公告管理',
     icon: 'HiOutlineSpeakerphone',
     path: '/announce',
     pages: {
       news: { name: '最新消息', path: '/announce/news' },
+      banner: { name: '首页轮播图', path: '/announce/banner' },
       marquee: { name: '跑马灯', path: '/announce/marquee' },
       message: { name: '站内信', path: '/announce/message' },
-    },
-  },
-  activity: {
-    name: '优惠活动',
-    icon: 'HiOutlineTag',
-    path: '/activity',
-    pages: {
-      activity: { name: '活动管理', path: '/activity/manage' },
-      activityReview: { name: '活动审核', path: '/activity/review' },
-    },
-  },
-  sport: {
-    name: '体育设定',
-    icon: 'HiOutlineAdjustments',
-    path: '/sport',
-    pages: {
-      country: { name: '国家管理', path: '/sport/country' },
-      sport: { name: '运动管理', path: '/sport/sport' },
-      game: { name: '球种管理', path: '/sport/game' },
-      leagueGroup: { name: '联盟群组管理', path: '/sport/league-group' },
-      league: { name: '联盟管理', path: '/sport/league' },
-      team: { name: '队伍管理', path: '/sport/team' },
-      odds: { name: '赔率管理', path: '/sport/odds' },
-      defaultBet: {
-        name: '預設下注設定',
-        path: '/sport/default-bet',
-        menuHidden: true,
-      },
-    },
-  },
-  website: {
-    name: '网站管理',
-    icon: 'HiOutlineDesktopComputer',
-    pages: {
-      banner: { name: '首页轮播图', path: '/website/banner' },
-      faqCategory: {
-        name: '常见问题分类',
-        path: '/website/faq/category',
-        menuHidden: true,
-      },
-      faq: { name: '常见问题', path: '/website/faq' },
-      pageContent: { name: '内容管理', path: '/website/page-content' },
     },
   },
   member: {
@@ -101,14 +50,21 @@ const menu: MenuTree = {
       member: { name: '会员列表', path: '/member/manage' },
       tag: { name: '会员标籤', path: '/member/tag' },
       bank: { name: '会员银行卡', path: '/member/bank' },
+      realName: { name: '實名認證', path: '/member/real-name' },
     },
   },
-  block: {
-    name: '黑名单管理',
-    icon: 'HiOutlineExclamation',
+  event: {
+    name: '赛事相关',
+    icon: 'HiOutlineLightningBolt',
     pages: {
-      ipBlock: { name: 'IP黑名单', path: '/block/ip' },
-      countryBlock: { name: '国家黑名单', path: '/block/country' },
+      manage: { name: '赛事列表', path: '/event/manage' },
+      betRecord: { name: '注单列表', path: '/event/bet-record' },
+      accounting: {
+        name: '赛事结帐',
+        path: '/event/accounting',
+        menuHidden: true,
+      },
+      monitor: { name: '赛事控盘', path: '/event/monitor', menuHidden: true },
     },
   },
   report: {
@@ -130,6 +86,36 @@ const menu: MenuTree = {
       withdraw: { name: '提領審核', path: '/trade/withdraw-rec' },
     },
   },
+
+  activity: {
+    name: '优惠活动',
+    icon: 'HiOutlineTag',
+    path: '/activity',
+    pages: {
+      activity: { name: '活动管理', path: '/activity/manage' },
+      activityReview: { name: '活动审核', path: '/activity/review' },
+    },
+  },
+
+  admin: {
+    name: '管理员管理',
+    icon: 'HiOutlineStar',
+    path: '/admin',
+    pages: {
+      adminUser: { name: '管理员列表', path: '/admin/user' },
+      adminRole: { name: '角色管理', path: '/admin/role' },
+      menu: { name: '选单管理', path: '/admin/menu', menuHidden: true },
+    },
+  },
+
+  block: {
+    name: '黑名单管理',
+    icon: 'HiOutlineExclamation',
+    pages: {
+      ipBlock: { name: 'IP黑名单', path: '/block/ip' },
+      countryBlock: { name: '国家黑名单', path: '/block/country' },
+    },
+  },
   merchant: {
     name: '商户管理',
     icon: 'HiOutlineBriefcase',
@@ -137,18 +123,37 @@ const menu: MenuTree = {
       manage: { name: '商户列表', path: '/merchant/manage' },
     },
   },
-  event: {
-    name: '赛事相关',
-    icon: 'HiOutlineLightningBolt',
+  website: {
+    name: '网站管理',
+    icon: 'HiOutlineDesktopComputer',
     pages: {
-      manage: { name: '赛事列表', path: '/event/manage' },
-      betRecord: { name: '注单列表', path: '/event/bet-record' },
-      accounting: {
-        name: '赛事结帐',
-        path: '/event/accounting',
+      faqCategory: {
+        name: '常见问题分类',
+        path: '/website/faq/category',
         menuHidden: true,
       },
-      monitor: { name: '赛事控盘', path: '/event/monitor', menuHidden: true },
+      faq: { name: '常见问题', path: '/website/faq' },
+      pageContent: { name: '内容管理', path: '/website/page-content' },
+    },
+  },
+
+  sport: {
+    name: '体育设定',
+    icon: 'HiOutlineAdjustments',
+    path: '/sport',
+    pages: {
+      country: { name: '国家管理', path: '/sport/country' },
+      sport: { name: '运动管理', path: '/sport/sport' },
+      game: { name: '球种管理', path: '/sport/game' },
+      leagueGroup: { name: '联盟群组管理', path: '/sport/league-group' },
+      league: { name: '联盟管理', path: '/sport/league' },
+      team: { name: '队伍管理', path: '/sport/team' },
+      odds: { name: '赔率管理', path: '/sport/odds' },
+      defaultBet: {
+        name: '預設下注設定',
+        path: '/sport/default-bet',
+        menuHidden: true,
+      },
     },
   },
 }
