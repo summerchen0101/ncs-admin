981c3c59ed5d003aaa188e9ae800a427c7460898
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-07 18:36:52 +0800

    theme: updated

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,13 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Center, Text } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 
 const Logo: React.FC = () => {
   return (
-    <Box pb="15px" pt="18px" textAlign="center">
+    <Center textAlign="center" h="60px">
       <Text color="white" fontSize="20px" letterSpacing="3px" fontWeight="500">
         體育反波膽
       </Text>
-    </Box>
+    </Center>
   )
 }
 
