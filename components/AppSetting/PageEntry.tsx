8ffda962dc6a7338721eb84be9f1619a973d2a57
Summer Chen <mp4bj6ruby@gmail.com>
2021-04-03 18:30:37 +0800

    AppSetting: added serviceUrl

--- components/AppSetting/PageEntry.tsx
+++ components/AppSetting/PageEntry.tsx
@@ -1,12 +1,11 @@
 import { useDataContext } from '@/context/DataContext'
 import { AppSetting } from '@/types/api/AppSetting'
 import useAppSettingService from '@/utils/services/useAppSettingService'
-import { Box, SimpleGrid } from '@chakra-ui/layout'
-import { useToast } from '@chakra-ui/toast'
+import { Box } from '@chakra-ui/layout'
 import { Button, Form, Input } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
 import { useRouter } from 'next/dist/client/router'
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect } from 'react'
 import Dashboard from '../Dashboard'
 
 interface AppSettingFormProps extends AppSetting {}
@@ -16,19 +15,10 @@ const PageEntry: React.FC = () => {
   const [form] = useForm<AppSettingFormProps>()
   const { fetchById, doEdit } = useAppSettingService()
   const { viewData } = useDataContext<AppSetting>()
-  const formData = useMemo<AppSettingFormProps>(() => {
-    return {
-      bank_card_limit: viewData.bank_card_limit,
-      bet_fee_percent: viewData.bet_fee_percent,
-      bet_confirm_min: viewData.bet_confirm_min,
-    }
-  }, [viewData])
-  const toast = useToast()
 
   const onSubmit = async (values: AppSettingFormProps) => {
     try {
       await doEdit(values)
-      toast({ status: 'success', title: '修改成功' })
     } catch (err) {
       console.log(err)
     }
@@ -37,15 +27,14 @@ const PageEntry: React.FC = () => {
     fetchById()
   }, [])
 
+  useEffect(() => {
+    form.setFieldsValue(viewData)
+  }, [viewData])
+
   return (
     <Dashboard>
       <Box w={[null, '300px']}>
-        <Form
-          layout="vertical"
-          form={form}
-          initialValues={formData}
-          onFinish={onSubmit}
-        >
+        <Form layout="vertical" form={form} onFinish={onSubmit}>
           <Form.Item
             label="银行卡最大张数"
             name="bank_card_limit"
@@ -66,6 +55,13 @@ const PageEntry: React.FC = () => {
             rules={[{ required: true }]}
           >
             <Input addonAfter="分" />
+          </Form.Item>
+          <Form.Item
+            label="客服连结"
+            name="live_help_url"
+            rules={[{ required: true }]}
+          >
+            <Input />
           </Form.Item>
           <Button htmlType="submit">确认送出</Button>
         </Form>
--- types/api/AppSetting.ts
+++ types/api/AppSetting.ts
@@ -2,6 +2,7 @@ export interface AppSetting {
   bank_card_limit: number
   bet_fee_percent: number
   bet_confirm_min: number
+  live_help_url: string
 }
 
 export interface AppSettingEditRequest extends AppSetting {}
