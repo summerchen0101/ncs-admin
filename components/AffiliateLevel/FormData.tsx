989ad64e6a4db8817a01fec804ef0a6ed19f0894
Summer Chen <mp4bj6ruby@gmail.com>
2021-04-11 18:39:19 +0800

    AffiliateLevel: form validation

--- components/AffiliateLevel/FormData.tsx
+++ components/AffiliateLevel/FormData.tsx
@@ -35,28 +35,52 @@ function FormData({
         <Form.Item label="阶级名称" name="name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
-        <Form.Item label="阶级排序" name="level">
+        <Form.Item label="阶级排序" name="level" rules={[{ required: true }]}>
           <InputNumber style={{ width: '100%' }} />
         </Form.Item>
-        <Form.Item label="活跃玩家数" name="active_member_count">
+        <Form.Item
+          label="活跃玩家数"
+          name="active_member_count"
+          rules={[{ required: true }]}
+        >
           <Input addonAfter="个" />
         </Form.Item>
-        <Form.Item label="有效代理数" name="active_agent_count">
+        <Form.Item
+          label="有效代理数"
+          name="active_agent_count"
+          rules={[{ required: true }]}
+        >
           <Input addonAfter="个" />
         </Form.Item>
       </SimpleGrid>
 
       <SimpleGrid columns={2} spacingX="20px">
-        <Form.Item label="会员输赢" name="result_min">
+        <Form.Item
+          label="会员输赢"
+          name="result_min"
+          rules={[{ required: true }]}
+        >
           <CurrencyInputNumber style={{ width: '100%' }} step={100} min={0} />
         </Form.Item>
-        <Form.Item label="会员输赢佣金" name="result_percent">
+        <Form.Item
+          label="会员输赢佣金"
+          name="result_percent"
+          rules={[{ required: true }]}
+        >
           <Input addonAfter="％" />
         </Form.Item>
-        <Form.Item label="累计手续费" name="fee_min">
+        <Form.Item
+          label="累计手续费"
+          name="fee_min"
+          rules={[{ required: true }]}
+        >
           <CurrencyInputNumber style={{ width: '100%' }} step={100} min={0} />
         </Form.Item>
-        <Form.Item label="手续费佣金" name="fee_percent">
+        <Form.Item
+          label="手续费佣金"
+          name="fee_percent"
+          rules={[{ required: true }]}
+        >
           <Input addonAfter="％" />
         </Form.Item>
         <Form.Item label="状态" name="is_active" valuePropName="checked">
