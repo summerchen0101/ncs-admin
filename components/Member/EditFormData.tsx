3b08953298bf2864e3f111af3664607529394f01
Summer Chen <mp4bj6ruby@gmail.com>
2021-04-14 22:07:53 +0800

    EditFormData: agent rm promo_level field

--- components/Member/EditFormData.tsx
+++ components/Member/EditFormData.tsx
@@ -63,20 +63,24 @@ function EditFormData({
         <Form.Item label="会员备注" name="note">
           <Input />
         </Form.Item>
-        <Form.Item
-          label="佣金等级"
-          name="promo_level"
-          rules={[{ required: true }]}
-        >
-          <Select options={affiliateLevelOpts} placeholder="请选择" />
-        </Form.Item>
-        <Form.Item
-          label="佣金等级锁定"
-          name="is_lock_promo_level"
-          valuePropName="checked"
-        >
-          <Switch />
-        </Form.Item>
+        {data.member_type === MemberType.Member && (
+          <>
+            <Form.Item
+              label="佣金等级"
+              name="promo_level"
+              rules={[{ required: true }]}
+            >
+              <Select options={affiliateLevelOpts} placeholder="请选择" />
+            </Form.Item>
+            <Form.Item
+              label="佣金等级锁定"
+              name="is_lock_promo_level"
+              valuePropName="checked"
+            >
+              <Switch />
+            </Form.Item>
+          </>
+        )}
       </SimpleGrid>
     </Form>
   )
