23efdaa0e47b3c55a35014d1641492c04c1ba239
Summer Chen <mp4bj6ruby@gmail.com>
2021-04-26 14:33:13 +0800

    ActivityReview: bonus allow modify

--- components/ActivityReview/EditPopup.tsx
+++ components/ActivityReview/EditPopup.tsx
@@ -6,17 +6,23 @@ import { ActivityReview } from '@/types/api/ActivityReview'
 import useActivityReviewService from '@/utils/services/useActivityReviewService'
 import useTransfer from '@/utils/useTransfer'
 import { HStack } from '@chakra-ui/layout'
-import { Button, Descriptions, Modal } from 'antd'
-import React, { useEffect } from 'react'
+import { Button, Descriptions, InputNumber, Modal } from 'antd'
+import React, { useEffect, useState } from 'react'
 
 function EditPopup() {
   const { setStatus } = useActivityReviewService()
   const [visible, setVisible] = usePopupContext('editForm')
   const { viewData } = useDataContext<ActivityReview>()
+  const [bonus, setBonus] = useState(0)
+
   const { toCurrency, toDateTime } = useTransfer()
   const handleSubmit = async () => {
     try {
-      await setStatus(viewData.id, ProcessStatus.Finish)
+      await setStatus({
+        id: viewData.id,
+        status: ProcessStatus.Finish,
+        bonus: bonus,
+      })
       setVisible(false)
     } catch (err) {}
   }
@@ -25,10 +31,17 @@ function EditPopup() {
   }
   const handleReject = async () => {
     try {
-      await setStatus(viewData.id, ProcessStatus.Cancel)
+      await setStatus({
+        id: viewData.id,
+        status: ProcessStatus.Cancel,
+      })
       setVisible(false)
     } catch (err) {}
   }
+
+  useEffect(() => {
+    viewData?.bonus && setBonus(viewData?.bonus)
+  }, [viewData])
   if (!viewData) return <></>
   return (
     <Modal
@@ -55,7 +68,11 @@ function EditPopup() {
           {viewData.member.acc} [{viewData.member.name}]
         </Descriptions.Item>
         <Descriptions.Item label="活动奖金">
-          ${toCurrency(viewData.bonus)}
+          <InputNumber
+            style={{ width: '100%' }}
+            value={bonus}
+            onChange={(v) => setBonus(+v)}
+          />
         </Descriptions.Item>
         <Descriptions.Item label="申请时间">
           {toDateTime(viewData.created_at)}
