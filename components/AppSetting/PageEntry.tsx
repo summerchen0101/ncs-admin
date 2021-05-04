55f329a33c5f049353c31fa678eba264eb6615c1
Summer Chen <mp4bj6ruby@gmail.com>
2021-05-04 16:30:58 +0800

    AppSetting: api updated, added it to menu

--- components/AppSetting/PageEntry.tsx
+++ components/AppSetting/PageEntry.tsx
@@ -1,11 +1,21 @@
 import { useDataContext } from '@/context/DataContext'
 import { AppSetting } from '@/types/api/AppSetting'
 import useAppSettingService from '@/utils/services/useAppSettingService'
-import { Box } from '@chakra-ui/layout'
-import { Button, Form, Input } from 'antd'
+import { Button } from '@chakra-ui/button'
+import Icon from '@chakra-ui/icon'
+import {
+  Box,
+  Divider,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/layout'
+import { Form, Input, InputNumber } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
 import { useRouter } from 'next/dist/client/router'
 import React, { useEffect } from 'react'
+import { HiOutlineCog } from 'react-icons/hi'
 import Dashboard from '../Dashboard'
 
 interface AppSettingFormProps extends AppSetting {}
@@ -33,29 +43,35 @@ const PageEntry: React.FC = () => {
 
   return (
     <Dashboard>
-      <Box w={[null, '300px']}>
+      <Box w={[null, '400px']}>
+        <HStack my="15px" fontSize="20px" fontWeight="600" spacing="1">
+          <HiOutlineCog fontSize="25px" />
+          <Text>网站设置值</Text>
+        </HStack>
         <Form layout="vertical" form={form} onFinish={onSubmit}>
-          <Form.Item
-            label="银行卡最大张数"
-            name="bank_card_limit"
-            rules={[{ required: true }]}
-          >
-            <Input addonAfter="张" />
-          </Form.Item>
-          <Form.Item
-            label="注单手续费%"
-            name="bet_fee_percent"
-            rules={[{ required: true }]}
-          >
-            <Input addonAfter="%" />
-          </Form.Item>
-          <Form.Item
-            label="下注后可撤单时间"
-            name="bet_confirm_min"
-            rules={[{ required: true }]}
-          >
-            <Input addonAfter="分" />
-          </Form.Item>
+          <SimpleGrid columns={2} spacingX="20px">
+            <Form.Item
+              label="银行卡最大张数"
+              name="bank_card_limit"
+              rules={[{ required: true }]}
+            >
+              <Input addonAfter="张" />
+            </Form.Item>
+            <Form.Item
+              label="注单手续费%"
+              name="bet_fee_percent"
+              rules={[{ required: true }]}
+            >
+              <Input addonAfter="%" />
+            </Form.Item>
+            <Form.Item
+              label="下注后可撤单时间"
+              name="bet_confirm_min"
+              rules={[{ required: true }]}
+            >
+              <Input addonAfter="分" />
+            </Form.Item>
+          </SimpleGrid>
           <Form.Item
             label="客服连结"
             name="live_help_url"
@@ -63,7 +79,29 @@ const PageEntry: React.FC = () => {
           >
             <Input />
           </Form.Item>
-          <Button htmlType="submit">确认送出</Button>
+          <Form.Item label="线上人数" rules={[{ required: true }]}>
+            <HStack as={Input.Group}>
+              <Form.Item
+                name="online_count_min"
+                noStyle
+                rules={[{ required: true }]}
+              >
+                <InputNumber style={{ width: '100%' }} />
+              </Form.Item>
+              <Text>~</Text>
+              <Form.Item
+                name="online_count_max"
+                noStyle
+                rules={[{ required: true }]}
+              >
+                <InputNumber style={{ width: '100%' }} />
+              </Form.Item>
+            </HStack>
+          </Form.Item>
+
+          <Button type="submit" w="full" colorScheme="brand" borderRadius="sm">
+            确认修改
+          </Button>
         </Form>
       </Box>
     </Dashboard>
